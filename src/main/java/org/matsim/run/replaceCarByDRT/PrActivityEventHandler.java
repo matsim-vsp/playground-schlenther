a2f988e4747bcf225e9b1b6b07c724eb5b6ab8e2
ln0455686 <nitsch@campus.tu-berlin.de>
2023-08-18 17:41:46 +0200

    fixed bugs & tested PrActivityEventHandler

--- src/main/java/org/matsim/run/replaceCarByDRT/PrActivityEventHandler.java
+++ src/main/java/org/matsim/run/replaceCarByDRT/PrActivityEventHandler.java
@@ -1,7 +1,6 @@
 package org.matsim.run.replaceCarByDRT;
 
 import org.apache.commons.lang3.mutable.MutableInt;
-import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.TransportMode;
@@ -22,16 +21,9 @@ import java.util.Set;
  * vehicles are assumed to be parked or picked up at P+R stations depending on whether the routing mode before/after
  * the P+R activity was TransportMode.car or not.
  */
-class PrActivityEventHandler implements /*ActivityStartEventHandler,*/ ActivityEndEventHandler, PersonDepartureEventHandler {
+class PrActivityEventHandler implements ActivityEndEventHandler, PersonDepartureEventHandler {
 
-    private static final Logger log = Logger.getLogger(PrActivityEventHandler.class);
-
-    //key = name, value = coord
     private HashMap<PRStation, MutableInt> activitiesPerPRStation = new HashMap<>();
-
-//    private Map<PRStation, int[]> prStartsPerHour = new HashMap<>();
-
-//    private Map<PRStation, int[]> prActivitiesPerMinute = new HashMap<>();
 
     private Map<PRStation, int[]> carsInPrStationPerMinute = new HashMap<>();
 
@@ -39,54 +31,95 @@ class PrActivityEventHandler implements /*ActivityStartEventHandler,*/ ActivityE
 
     private Map<PRStation, Set<Id<Person>>> station2Users = new HashMap<>();
 
+    private Map<Id<Person>, Map.Entry<PRStation,Integer>> possibleRiders = new HashMap<>();
+
 
     public PrActivityEventHandler(Set<PRStation> prStations) {
         for (PRStation station : prStations){
             this.activitiesPerPRStation.put(station, new MutableInt(0));
             this.carsInPrStationPerMinute.put(station, new int[36*60]);
+            this.station2Users.put(station, new HashSet<>());
         }
     }
 
     @Override
     public void handleEvent(PersonDepartureEvent event) {
+
         this.person2LastRoutingMode.put(event.getPersonId(), event.getRoutingMode());
+
+            //checks if person could be a rider after a station
+            if(this.possibleRiders.containsKey(event.getPersonId())){
+
+                if(event.getRoutingMode().equals(TransportMode.ride)){
+                    //person uses ride after the station. All changes need to be undone.
+                    this.activitiesPerPRStation.get(possibleRiders.get(event.getPersonId()).getKey()).decrement();
+                    this.station2Users.remove(event.getPersonId());
+                    for (int i = possibleRiders.get(event.getPersonId()).getValue() - 1; i >= 0; i--) {
+                        carsInPrStationPerMinute.get(possibleRiders.get(event.getPersonId()).getKey())[i] --;
+                    }
+
+                    this.possibleRiders.remove(event.getPersonId());
+
+                } else {
+                    //person does not use ride after the station
+                    this.possibleRiders.remove(event.getPersonId());
+                }
+            }
     }
 
     @Override
     public void handleEvent(ActivityEndEvent event) {
-        if(event.getActType().equals("P+R")){
+           if(event.getActType().equals("P+R")){
 
-            int endMinute = (int) Math.floor(event.getTime() / 60);
+                int endMinute = (int) Math.floor(event.getTime() / 60);
 
-            //determine which station
-            PRStation prStation = getPRStationWithCoord(event.getCoord());
-            if(prStation == null){
-                throw new IllegalArgumentException("could not find P+R station with coord = " + event.getCoord() + "!! \n The following event happens there: " + event);
-            } else {
+                //determine which station
+                PRStation prStation = getPRStationWithCoord(event.getCoord());
+                if(prStation == null){
+                    throw new IllegalArgumentException("could not find P+R station with coord = " + event.getCoord() + "!! \n The following event happens there: " + event);
+                }
+
+                // agents who ride to the station get skipped
+                if(!person2LastRoutingMode.get(event.getPersonId()).equals(TransportMode.ride)){
+
                     //determine whether vehicle is about to be parked or to be picked up
                     if(person2LastRoutingMode.get(event.getPersonId()).equals(TransportMode.car)){
                         //person arrives with a car, thus car is now about to get parked
                         for (int i = endMinute; i < 36*60; i++) {
                             carsInPrStationPerMinute.get(prStation)[i] ++;
                         }
+
+                        //person now gets tracked that car is at this station
+                        this.station2Users.get(prStation).add(event.getPersonId());
+
                     } else {
+
                         //vehicle is picked up
                         if(this.station2Users.get(prStation).contains(event.getPersonId())) {
                             for (int i = endMinute; i < 36 * 60; i++) {
                                 carsInPrStationPerMinute.get(prStation)[i]--;
                             }
+
+                            //person now gets removed that car is not at this station anymore
+                            this.station2Users.get(prStation).remove(event.getPersonId());
+
                         } else {
-                            //person was not tracked at this station before (even though we pick up the car)
+                            // Case 1: person was not tracked at this station before (even though we pick up the car)
                             // -> we are tracking a resident of the ban area and have to add the car for all previous time bins
                             for (int i = endMinute - 1; i >= 0; i--) {
                                 carsInPrStationPerMinute.get(prStation)[i] ++;
                             }
+
+                            //person now gets tracked that car is at this station
+                            this.station2Users.get(prStation).add(event.getPersonId());
+
+                            // Case 2: person uses mode ride after the station. Gets handled in first personDepartureEvent of the person after the station
+                            this.possibleRiders.put(event.getPersonId(), Map.entry(prStation,endMinute));
                         }
                     }
+                    this.activitiesPerPRStation.get(prStation).increment();
                 }
-            this.station2Users.get(prStation).add(event.getPersonId());
-            this.activitiesPerPRStation.get(prStation).increment();
-        }
+           }
     }
 
     @Nullable
--- src/main/java/org/matsim/run/replaceCarByDRT/RunPRActivityEventHandler.java
+++ src/main/java/org/matsim/run/replaceCarByDRT/RunPRActivityEventHandler.java
@@ -2,14 +2,9 @@ package org.matsim.run.replaceCarByDRT;
 
 import com.opencsv.CSVWriter;
 import org.apache.commons.lang3.mutable.MutableInt;
-import org.apache.log4j.Logger;
-import org.matsim.analysis.RunTripsPreparation;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
-import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.utils.io.IOUtils;
 
 import java.io.IOException;
@@ -23,25 +18,19 @@ public class RunPRActivityEventHandler {
     private final String runId;
     private final String pr_stations;
 
-
-    private static final Logger log = Logger.getLogger(RunTripsPreparation.class);
-
-
     public RunPRActivityEventHandler(String runDirectory, String runId, String prStations) {
         this.runDirectory = runDirectory;
         this.runId = runId;
         this.pr_stations = prStations;
-
     }
-
 
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            String runDirectory = "scenarios/output/runs-2023-06-02/extraPtPlan-true/drtStopBased-true/massConservation-true/";
+            String runDirectory = "scenarios/output/runs-2023-08-11/stationChoice-closestToOutside/";
             //Cluster: scenarios/output/runs-2023-06-02/extraPtPlan-true/drtStopBased-true/massConservation-true/
-            String runId = "extraPtPlan-drtStopBased-1506vehicles-8seats";
-            String tsvFilePath = "scenarios/berlin/replaceCarByDRT/noModeChoice/prStations/2023-03-29-pr-stations.tsv";
+            String runId = "stationChoice-closestToOutside";
+            String tsvFilePath = "scenarios/berlin/replaceCarByDRT/noModeChoice/prStations/2023-07-27-pr-stations.tsv";
 
             RunPRActivityEventHandler prActivities = new RunPRActivityEventHandler(runDirectory, runId, tsvFilePath);
             prActivities.run();
@@ -57,9 +46,6 @@ public class RunPRActivityEventHandler {
 
     public void run(){
         String inputFile = runDirectory + runId + ".output_events.xml.gz";
-        String inputNetwork = runDirectory + runId + ".output_network.xml.gz";
-
-        Network network = NetworkUtils.readNetwork(inputNetwork);
 
         // read CSV file
         Set<PRStation> prStationsSet = ReplaceCarByDRT.readPRStationFile(IOUtils.resolveFileOrResource(pr_stations));
@@ -78,89 +64,26 @@ public class RunPRActivityEventHandler {
         events.finishProcessing();
 
         //write to CSV file
-        String outputFileName1 = inputFile.substring(0, inputFile.lastIndexOf(".xml")) + "_prStartsPerHour.tsv";
-        String outputFileName2 = inputFile.substring(0, inputFile.lastIndexOf(".xml")) + "_prActivitiesPerMinute.tsv";
         String outputFileName3 = inputFile.substring(0, inputFile.lastIndexOf(".xml")) + "_activitiesPerPRStation.tsv";
         String outputFileName4 = inputFile.substring(0, inputFile.lastIndexOf(".xml")) + "_carsInPrStationPerMinute.tsv";
-        agentsPerPRStation2CSV(handler1.getActivitiesPerPRStation(), network, outputFileName3);
-//        prStartsPerHour2CSV(handler1.getPrStartsPerHour(),outputFileName1);
-//        prActivitiesPerMinute2CSV(handler1.getPrActivitiesPerMinute(), outputFileName2);
+        agentsPerPRStation2CSV(handler1.getActivitiesPerPRStation(), outputFileName3);
         carsInPrStationPerMinute2CSV(handler1.getCarsInPrStationPerMinute(), outputFileName4);
     }
 
-    private static void agentsPerPRStation2CSV(Map<PRStation, MutableInt> prActivities, Network network, String outputFileName){
+    private static void agentsPerPRStation2CSV(Map<PRStation, MutableInt> prActivities, String outputFileName){
         try {
             CSVWriter writer = new CSVWriter(Files.newBufferedWriter(Paths.get(outputFileName)), '\t', CSVWriter.NO_QUOTE_CHARACTER, '"', "\n");
-            writer.writeNext(new String[]{"PRStation","Agents","x","y","AgentsByEdgeCapacity"});
+            writer.writeNext(new String[]{"PRStation","Agents","x","y"});
 
             for (Map.Entry<PRStation, MutableInt> entry : prActivities.entrySet()) {
                 PRStation station = entry.getKey();
                 MutableInt agentsPerPRStation = entry.getValue();
-                Link link = network.getLinks().get(station.getLinkId());
-                Double edgeCapacity = link.getCapacity();
-                Double agentsByEdgeCapacity = agentsPerPRStation.doubleValue() / (edgeCapacity * 36);
 
-                writer.writeNext(new String[]{station.getName(),String.valueOf(agentsPerPRStation),String.valueOf(station.coord.getX()),String.valueOf(station.coord.getY()),String.valueOf(agentsByEdgeCapacity)});
+
+                writer.writeNext(new String[]{station.getName(),String.valueOf(agentsPerPRStation),String.valueOf(station.coord.getX()),String.valueOf(station.coord.getY())});
             }
             writer.close();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-
-    private static void prStartsPerHour2CSV(Map<PRStation, int[]> prActivities, String outputFileName){
-        try {
-            CSVWriter writer = new CSVWriter(Files.newBufferedWriter(Paths.get(outputFileName)), '\t', CSVWriter.NO_QUOTE_CHARACTER, '"', "\n");
-
-            List<String> header = new ArrayList<String>();
-            header.add("PRStation");
-            for (int i = 0; i < 36; i++){
-                header.add(i + ":00 - " + i + ":59");
-            }
-            writer.writeNext(header.toArray(new String[0]));
-
-            for (Map.Entry<PRStation, int[]> entry : prActivities.entrySet()) {
-                PRStation station = entry.getKey();
-                int[] agentsPerHour = entry.getValue();
-                String[] agents = Arrays.toString(agentsPerHour).split("[\\[\\]]")[1].split(", ");
-                List<String> agentsList = new ArrayList<String>(Arrays.asList(agents));
-                agentsList.add(0, station.getName());
-
-                writer.writeNext(agentsList.toArray(new String[0]));
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static void prActivitiesPerMinute2CSV(Map<PRStation, int[]> prActivities, String outputFileName){
-        try {
-            CSVWriter writer = new CSVWriter(Files.newBufferedWriter(Paths.get(outputFileName)), '\t', CSVWriter.NO_QUOTE_CHARACTER, '"', "\n");
-
-            List<String> header = new ArrayList<String>();
-            header.add("PRStation");
-            for (int i = 0; i < 36; i++){
-                for(int j = 0; j < 60; j++) {
-                    header.add(i + ":" + j + ":00 - " + i + ":" + j + ":59");
-                }
-            }
-            writer.writeNext(header.toArray(new String[0]));
-
-            for (Map.Entry<PRStation, int[]> entry : prActivities.entrySet()) {
-                PRStation station = entry.getKey();
-                int[] agentsPerMinute = entry.getValue();
-                String[] agents = Arrays.toString(agentsPerMinute).split("[\\[\\]]")[1].split(", ");
-                List<String> agentsList = new ArrayList<String>(Arrays.asList(agents));
-                agentsList.add(0, station.getName());
-
-                writer.writeNext(agentsList.toArray(new String[0]));
-            }
-            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -175,7 +98,7 @@ public class RunPRActivityEventHandler {
             header.add("PRStation");
             for (int i = 0; i < 36; i++){
                 for(int j = 0; j < 60; j++) {
-                    header.add(i + ":" + j + ":00 - " + i + ":" + j + ":59");
+                    header.add(i + ":" + j);
                 }
             }
             writer.writeNext(header.toArray(new String[0]));
